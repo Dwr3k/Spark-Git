3d553e7f2152e0c9cb884c2b1c53391263e9b17a
dwr3k <mcgloneda@vcu.edu>
2023-08-01 11:45:12 -0400

    Connected to Postgres

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 ThisBuild / scalaVersion := "2.11.0"
 
 // https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-client
-libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "3.3.1"
+libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.7.1"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-core
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.7"
 // https://mvnrepository.com/artifact/commons-io/commons-io
@@ -11,8 +11,11 @@ libraryDependencies += "commons-io" % "commons-io" % "2.6"
 libraryDependencies += "org.slf4j" % "slf4j-api" % "1.7.2"
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
 libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.7"
-// https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
-libraryDependencies += "org.xerial" % "sqlite-jdbc" % "3.36.0.3"
+// https://mvnrepository.com/artifact/org.postgresql/postgresql
+libraryDependencies += "org.postgresql" % "postgresql" % "42.5.1"
+
+
+
 
 
 
--- src/main/scala/Melee.scala
+++ src/main/scala/Melee.scala
@@ -2,35 +2,76 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions.count
-
 object Melee{
 
   def main(args:Array[String]):Unit = {
-    val url = s"jdbc:postgresql://${args(0)}"
-    //val outputPath = args(1)
-//    val url = "jdbc:sqlite:C:\\Users\\Consultant\\Desktop\\melee_player_database.db"
-    //  System.setProperty("hadoop.home.dir", "C:\\Users\\Consultant\\Documents\\hadoop-2.8.1\\hadoop-2.8.1")
+    val url = s"jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com/testdb"
+//    val url = "jdbc:sqlite:C:\\Users\\Consultant\\Desktop\\DemoDB\\melee_player_database.db"
+//    System.setProperty("hadoop.home.dir", "C:\\Users\\Consultant\\Documents\\hadoop-2.8.1\\hadoop-2.8.1")
 
     Logger.getLogger("org").setLevel(Level.ERROR)
-    //println(s"URL == $url")
+    println(s"URL == $url")
 
     val sparkConf = new SparkConf()
 
-    //sparkConf.set("spark.master", "local")
-    sparkConf.set("spark.app.name", "Horse Pipeline")
+    sparkConf.set("spark.master", "local")
+    sparkConf.set("spark.app.name", "Melee Pipeline")
     val spark = SparkSession.builder().config(sparkConf).getOrCreate()
 
-//    val allDataDF = spark.read.option("header", true).option("delimiter", ";").csv(args(1))
+    val username: String = args(0)
+    val password: String = args(1)
 
-    val allData = spark.read.format("jdbc").options(Map("driver" -> "org.postgresql.jdbc", "url" -> url, "dbtable" -> "drem_horses", "usermame" -> args(1), "password" -> args(2))).load()
+    val players = spark.read.format("jdbc").options(Map("driver" -> "org.postgresql.Driver", "fetchsize" -> "10000","url" -> url.trim(), "dbtable" -> "players", "user" -> username, "password" -> password)).load()
+    val sets = spark.read.format("jdbc").options(Map("driver" -> "org.postgresql.Driver", "fetchsize" -> "10000", "url" -> url.trim(), "dbtable" -> "sets", "user" -> username, "password" -> password)).load()
+    val tournamentInfo = spark.read.format("jdbc").options(Map("driver" -> "org.postgresql.Driver","fetchsize" -> "10000", "url" -> url.trim(), "dbtable" -> "tournament_info", "user" -> username, "password" -> password)).load()
 
-    //select jockey, horseid, count(jockey) as num_races, round(avg("Final place")) average_finish from drem_horses group by jockey,horseid order by num_races desc, average_finish asc ;
-    allData.show()
+    players.show()
+    sets.show()
+    tournamentInfo.show()
+
+
+//    players.createTempView("players")
+//    sets.createTempView("sets")
+//    tournamentInfo.createTempView("tournament_info")
+//
+//    val gf1 = spark.sql("Select p1.tag, p2.tag, count(*) as times_won " +
+//      "from sets s join players w on s.winner_id = w.player_id " +
+//      "join players p1 on p1.player_id = s.p1_id " +
+//      "join players p2 on p2.player_id = s.p2_id " +
+//      "where (p1.tag = \"Leffen\" and p2.tag = \"Armada\") or (p1.tag = \"Armada\" and p2.tag = \"Leffen\") \ngroup by p1.tag, p2.tag\norder by times_won DESC")
+//    //gf1.repartition(1).write.mode("overwrite").csv(args(2))
+//    gf1.show()
+//
+//
+//    val gf2 = spark.sql("select w.tag as Winner, p1.tag as Player_1, p2.tag as Player_2, p1_score, p2_score, tournament_key from sets join players w on winner_id = w.player_id " +
+//      "join players p1 on p1_id = p1.player_id\njoin players p2 on p2_id = p2.player_id\njoin tournament_info ti on ti.key = sets.tournament_key " +
+//      "where sets.location_names = '[\"GFR\", \"GF Reset\", \"Grand Final Reset\"]'  or sets.location_names = '[\"GF\", \"Grand Final\", \"Grand Final\"]'\norder by ti.start desc")
+//    //gf2.repartition(1).write.mode("overwrite").csv()
+//    gf2.show()
+//
+//
+//    val gf3 = spark.sql("select state, count(state) as tourneys_held from tournament_info where online == 0 group by state order by tourneys_held desc")
+//    gf3.show()
 
 
 
-//    val meleeSets = spark.read.format("jdbc").options(Map("driver" -> "org.sqlite.JDBC", "url" -> url.trim(), "dbtable" -> "(select * from players)")).load()
-//    val meleePlayers = spark.read.format("jdbc").options(Map("driver" -> "org.sqlite.JDBC", "url" -> url.trim(), "dbtable" -> "players")).load()
+
+
+
+
+
+
+
+////    val allDataDF = spark.read.option("header", true).option("delimiter", ";").csv(args(1))
+//
+//    val allData = spark.read.format("jdbc").options(Map("driver" -> "org.postgresql.jdbc", "url" -> url, "dbtable" -> "drem_horses", "usermame" -> args(1), "password" -> args(2))).load()
+//
+//    //select jockey, horseid, count(jockey) as num_races, round(avg("Final place")) average_finish from drem_horses group by jockey,horseid order by num_races desc, average_finish asc ;
+//    allData.show()
+
+
+
+
 
 
 
